18585b6c6b19f272785908757d939ffd646117c9
Livecate <johnlockwood@me.com>
2021-05-01 10:17:14 +0100

    chore: Split webpack into dev and prod builds

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,17 +4,16 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-    entry: './src/client/index.js'
-};
-
-module: {
-    rules: [
-        {
-            test:'/\.js$/',
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        }
-    ]
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test:'/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
